805b66c0f7c9ddde58d38ee937ad13602cf5f198
CleverBoy777 <boyclever777@gmail.com>
2022-10-05 17:56:06 +0500

    Bismillah

---
+++ src/main/java/comfy/com/comfyapp/ComfyAppApplication.java
@@ -0,0 +1,13 @@
+package comfy.com.comfyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComfyAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComfyAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/comfy/com/comfyapp/ComfyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package comfy.com.comfyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComfyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
